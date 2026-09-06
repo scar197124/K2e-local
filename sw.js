101b8b8f0ec3fc905c4f2ffddf28cb7ba173da27
scar197124 <spartica1791@gmail.com>
2026-09-06 19:29:29 +0000

    Apply restored artwork and compact portrait flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'k2e-local-v2.0.0-rc.45';
+const CACHE_NAME = 'k2e-local-v2.0.0-rc.45-art5';
 const APP_SHELL = [
   './','./index.html','./app.html','./manifest.json','./robots.txt','./sitemap.xml',
   './icon-192.png','./icon-512.png',
