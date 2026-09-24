d32a69de208cb32d5373e94113a6e5f1754bb3be
scar197124 <spartica1791@gmail.com>
2026-09-24 11:48:08 -0400

    Show device swipe hint only on narrow screens

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'k2e-local-v2.0.0-rc.45-device-header-icon1';
+const CACHE_NAME = 'k2e-local-v2.0.0-rc.45-device-header-icon2';
 const APP_SHELL = [
   './','./index.html','./app.html','./manifest.json','./robots.txt','./sitemap.xml',
   './icon-192.png','./icon-512.png','./apple-touch-icon.png',
